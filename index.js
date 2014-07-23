a00facbadc7fb3de482f5142f233bc5606cab7f1
Garry Yao <yaojun85@gmail.com>
2014-07-23 20:12:19 +0800

    object#protect

--- index.js
+++ index.js
@@ -17,6 +17,29 @@ Object.prototype.describe = function(props, descriptor) {
   });
 };
 
+/**
+ * create a new object, making the specified properties read-only.
+  * @param {String} props comma separated property list
+ */
+Object.prototype.readonly = function(props) {
+  var me = this;
+  props = props.split(',').reduce(function(obj, key) {
+    obj[key] = 1;
+    return obj;
+  },{});
+
+  function guardian(proxy, prop) {
+    if (prop in props) {
+      throw new ReferenceError("property \"" + prop + "\" is currently under protection");
+    }
+    return me[prop];
+  }
+
+  var handler = new FowardHandler(me);
+  handler.set = handler.get = guardian;
+  return Proxy.create(handler);
+};
+
 var obj = {
   id: 123,
   name: 'John',
@@ -34,14 +57,19 @@ obj.describe('maxage,updated,uid', { enumerable: false });
 console.log(Object.keys(obj));
 
 Object.preventExtensions(obj);
-obj.email = 'foo@bar.com';  // can't augment
+//obj.email = 'foo@bar.com';  // can't augment
 
 Object.seal(obj);
-delete obj.name;  // can't remove
+//delete obj.name;  // can't remove
 
 
 Object.freeze(obj);
-obj.name = 'Garry'; // can't mutate
+//obj.name = 'Garry'; // can't mutate
+
+var proxied = obj.readonly('maxage,updated,uid');
+
+Object.keys(proxied);
+console.log(proxied.uid); // can't read
 
 
 
